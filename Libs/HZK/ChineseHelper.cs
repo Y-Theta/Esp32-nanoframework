81cefd2ae906adacf4891d99b6e45c7a8bdcaa0e
杨 杰铭 <yangjm@mctech.vip>
2024-12-13 11:05:54 +0800

    add hzk16 text viewer

--- Libs/HZK/ChineseHelper.cs
+++ Libs/HZK/ChineseHelper.cs
@@ -5,19 +5,46 @@ namespace HZK
     public class ChineseHelper
     {
 
+        public int GetOffset(byte gb1, byte gb2)
+        {
+            return (int)(94 * (gb1 - 1) + (gb2 - 1)) * 32;
+        }
+
+        /// <summary>
+        /// 获取 gb2312 码的区位对应的在字库文件中的偏移
+        /// </summary>
+        /// <param name="gb2312"></param>
+        /// <returns></returns>
+        public int[] GetOffset(byte[] gb2312)
+        {
+            if (gb2312 is null)
+                return new int[0];
+
+            int size = gb2312.Length / 2;
+            if (size == 0)
+                return new int[0];
+
+            var result = new int[size];
+            for (int i = 0; i < gb2312.Length;)
+            {
+                result[i / 2] = GetOffset(gb2312[i], gb2312[i + 1]);
+                i += 2;
+            }
+            return result;
+        }
+
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="bytes"></param>
+        /// <param name="source"></param>
         /// <returns></returns>
-        public int GetOffset(byte gb1, byte gb2)
+        public byte[] Utf2Gb2312Byte(byte[] source)
         {
-            int offset = (int)(94 * (gb1 - 1) + (gb2 - 1)) * 32;
-            return offset;
+            if (source is null)
+                return new byte[0];
+
+            return new byte[0];
         }
-
-
-
 
     }
 }
--- Test(OnWindows)/test-HZ-print/MainWindow.xaml.cs
+++ Test(OnWindows)/test-HZ-print/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using HzRes;
 
+using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -21,9 +23,64 @@ namespace test_HZ_print
         public MainWindow()
         {
             InitializeComponent();
+        }
 
-            var hzk16 = HzRes.Resource.hzk16h;
-            
+        public int GetOffset(byte gb1, byte gb2)
+        {
+            return (int)(94 * (gb1 - 1) + (gb2 - 1)) * 32;
+        }
+
+        private readonly object _lock = new object();
+        private async void GB1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            await DrawCode();
+        }
+
+        private async Task DrawCode()
+        {
+            int pixelsize = 4;
+            await Task.CompletedTask;
+            lock (_lock)
+            {
+                try
+                {
+                    CANVAS.BeginInit();
+                    CANVAS.Children.Clear();
+                    byte.TryParse(GB1.Text, out var gb1);
+                    byte.TryParse(GB2.Text, out var gb2);
+                    var offset = GetOffset(gb1, gb2);
+                    int row = -pixelsize;
+                    int col = 0;
+                    for (int i = 0; i < 32; i++)
+                    {
+                        if (i % 2 == 0)
+                        {
+                            row += pixelsize;
+                            col = 0;
+                        }
+
+                        var code = HzRes.Resource.hzk16h[offset + i];
+                        for (int o = 7; o >= 0; o--)
+                        {
+                            var bit = (code >> o) & 0b1;
+                            if (bit > 0)
+                            {
+                                var rect = new Rectangle { Fill = Brushes.Black, Width = pixelsize, Height = pixelsize };
+                                CANVAS.Children.Add(rect);
+                                rect.SetValue(Canvas.LeftProperty, (double)col);
+                                rect.SetValue(Canvas.TopProperty, (double)row);
+                            }
+                            col += pixelsize;
+                        }
+                    }
+
+                }
+                catch { }
+                finally
+                {
+                    CANVAS.EndInit();
+                }
+            }
         }
     }
 }
