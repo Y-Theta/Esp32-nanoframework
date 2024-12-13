55d51d69a1cdcc699fabf7131d5712757e32b298
杨 杰铭 <yangjm@mctech.vip>
2024-12-13 09:39:59 +0800

    add some hz print functions

---
+++ Libs/HZK/ChineseHelper.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace HZK
+{
+    public class ChineseHelper
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        public int GetOffset(byte gb1, byte gb2)
+        {
+            int offset = (int)(94 * (gb1 - 1) + (gb2 - 1)) * 32;
+            return offset;
+        }
+
+
+
+
+    }
+}
---
+++ Libs/HzRes/ResourceManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HzRes
+{
+    public class ResourceManager
+    {
+
+        public const string RES_MAIN = nameof(Resource);
+
+        public const string HZK16H = nameof(Resource.hzk16h);
+        public const string UTF2GB2312 = nameof(Resource.utf2gb2312);
+
+
+        public static byte[] GetResource(string rescontainer, string resName)
+        {
+            var types = typeof(ResourceManager).Assembly.GetTypes();
+            var container = types.FirstOrDefault(type => type.Name == rescontainer);
+            if (container is null)
+                return new byte[0];
+
+            var res = container.GetProperty(resName, System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
+            if (res != null)
+            {
+                return res.GetValue(null) as byte[];
+            }
+
+            return new byte[0];
+        }
+    }
+}
