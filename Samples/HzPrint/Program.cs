23ee629b97eb264c41e9531586ce9fc3e8642f11
杨杰铭 <yangjm@mctech.vip>
2024-12-18 20:44:05 +0800

    ssd 1681 154 d 67

--- Samples/HzPrint/Program.cs
+++ Samples/HzPrint/Program.cs
@@ -1,3 +1,5 @@
+using Iot.Device.EPaper.Enums;
+using Iot.Device.EPaper;
 using Iot.Device.Ssd13xx;
 
 using nanoFramework.Hardware.Esp32;
@@ -5,7 +7,9 @@ using nanoFramework.Hardware.Esp32;
 using NF_Hanz;
 
 using System;
+using System.Device.Gpio;
 using System.Device.I2c;
+using System.Device.Spi;
 using System.Diagnostics;
 using System.Resources;
 using System.Text;
@@ -23,6 +27,42 @@ namespace HzPrint
             Configuration.SetPinFunction(22, DeviceFunction.I2C1_CLOCK);
             Configuration.SetPinFunction(21, DeviceFunction.I2C1_DATA);
 
+            Configuration.SetPinFunction(15, DeviceFunction.SPI1_CLOCK);
+            Configuration.SetPinFunction(2, DeviceFunction.SPI1_MOSI);
+
+            using var gpioController = new GpioController(PinNumberingScheme.Logical);
+            // Setup SPI connection with the display
+            var spiConnectionSettings = new SpiConnectionSettings(busId: 1, chipSelectLine: 18)
+            {
+                ClockFrequency = 20000000,
+                Mode = SpiMode.Mode1,
+                ChipSelectLineActiveState = false,
+                Configuration = SpiBusConfiguration.HalfDuplex,
+                DataFlow = DataFlow.MsbFirst,
+            };
+
+            using var spiDevice = new SpiDevice(spiConnectionSettings);
+            // Create an instance of the display driver
+            using var display = new Ssd1681_154D67(
+                spiDevice,
+                resetPin: 4,
+                busyPin: 19,
+                dataCommandPin: 5,
+                width: 200,
+                height: 200,
+                gpioController,
+                enableFramePaging: false,
+                shouldDispose: false);
+
+            display.PowerOn();
+            display.Initialize();
+            using var gfx = new Graphics(display)
+            {
+                DisplayRotation = Rotation.Degrees90Clockwise
+            };
+            display.Clear();
+            display.PerformFullRefresh();
+
             I2cDevice device = I2cDevice.Create(new I2cConnectionSettings(1, 0x3c, I2cBusSpeed.FastMode));
             Ssd1306 oled = new Ssd1306(device,Ssd13xx.DisplayResolution.OLED128x64);
             oled.Font = new hzFont();
@@ -30,8 +70,13 @@ namespace HzPrint
 
             while (true)
             {
-                oled.DrawString(0, 0, "我", 2);
+                oled.DrawString(0, 0, "一二三", 1);
                 oled.Display();
+
+                display.Clear();
+                DrawString("我喜欢宝宝", oled.Font, gfx);
+                display.Flush();
+                display.PerformFullRefresh();
                 Thread.Sleep(1000);
             }
 
@@ -42,6 +87,43 @@ namespace HzPrint
             // Join our lively Discord community: https://discord.gg/gCyBu8T
         }
 
+        public static void DrawString(string str, IFont font ,Graphics gfx)
+        {
+            int initc = 0;
+            int col = 0;
+            int row = 0;
+
+            foreach (char c in str)
+            {
+                var bytt = font[c];
+                for (int i = 0; i < bytt.Length;)
+                {
+                    if (i % 2 == 0)
+                    {
+                        row += 1;
+                        col = initc;
+                    }
+
+                    var item = bytt[i];
+                    for (int x = 0; x < 8; x++)
+                    {
+                        var bit = (item >> x) & 0b1;
+                        if (bit > 0)
+                        {
+                            gfx.DrawPixel(col, row, System.Drawing.Color.Black);
+                        }
+                        col += 1;
+                    }
+                    i++;
+                }
+
+                row = 0;
+                initc += 16;
+            }
+
+        }
+
+   
         public class hzFont : IFont
         {
             public override byte Height => 16;
---
+++ Samples/HzPrint/Ssd1681_154D67.cs
@@ -0,0 +1,147 @@
+﻿using Iot.Device.EPaper.Drivers.Ssd168x;
+
+using System;
+using System.Collections.Generic;
+using System.Device.Gpio;
+using System.Device.Spi;
+using System.Text;
+using System.Threading;
+
+namespace HzPrint
+{
+    internal class Ssd1681_154D67 : Ssd168x
+    {
+        protected override int PagesPerFrame => 1;
+
+        int waitsec = 20;
+        SpiDevice _device;
+        GpioController _pioController;
+
+        int _dataCommandPin = 0;
+        int _busyPin = 0;
+        public Ssd1681_154D67(SpiDevice spiDevice, int resetPin, int busyPin, int dataCommandPin, int width, int height, GpioController gpioController = null, bool enableFramePaging = true, bool shouldDispose = true) :
+            base(spiDevice, resetPin, busyPin, dataCommandPin, width, height, gpioController, enableFramePaging, shouldDispose)
+        {
+            _device = spiDevice;
+            _pioController = gpioController;
+            _dataCommandPin = dataCommandPin;
+            _busyPin = busyPin;
+        }
+
+
+        public override void SendData(params byte[] data)
+        {
+            _pioController.Write(_dataCommandPin, PinValue.High);
+
+            _device.Write(new SpanByte(data));
+
+            _pioController.Write(_dataCommandPin, PinValue.Low);
+        }
+
+        public override void Initialize()
+        {
+            if (PowerState != Iot.Device.EPaper.Enums.PowerState.PoweredOn)
+            {
+                throw new InvalidOperationException();
+            }
+
+            WaitMs(10);
+            SendCommand(0x12);
+            WaitMs(10);
+            SendCommand(0x01);
+            SendData(0xC7);
+            SendData(0x00);
+            SendData(0x00);
+            SendCommand(0x3C);
+            SendData(0x05);
+            SendCommand(0x18);
+            SendDataD(0x80);
+            SetParticalRam(0, 0, (UInt16)Width, (UInt16)Height);
+
+            PerformFullRefresh();
+        }
+
+        private void SetParticalRam(UInt16 x, UInt16 y, UInt16 w, UInt16 h)
+        {
+            SendCommand(0x11);
+            SendData(0x03);
+            SendCommand(0x44);
+            SendData((byte)(x / 8));
+            SendData((byte)((x + w - 1) / 8));
+            SendCommand(0x45);
+            SendData((byte)(y % 256));
+            SendData((byte)(y / 256));
+            SendData((byte)((y + h - 1) % 256));
+            SendData((byte)((y + h - 1) / 256));
+            SendCommand(0x4e);
+            SendData((byte)(x / 8));
+            SendCommand(0x4f);
+            SendData((byte)(y % 256));
+            SendData((byte)(y / 256));
+        }
+
+        public void SendDataD(params ushort[] data)
+        {
+            // set the data/command pin to high to indicate to the display we will be sending data
+            _pioController.Write(_dataCommandPin, PinValue.High);
+
+            _device.Write(data);
+
+            // go back to low (command mode)
+            _pioController.Write(_dataCommandPin, PinValue.Low);
+        }
+
+        public override bool PerformPartialRefresh()
+        {
+            uint x = 0, y = 0, w = (uint)Width, h = (uint)Height;
+            uint w1 = x < 0 ? w + x : w; // reduce
+            uint h1 = y < 0 ? h + y : h; // reduce
+            uint x1 = x < 0 ? 0 : x; // limit
+            uint y1 = y < 0 ? 0 : y; // limit
+            w1 = (uint)(x1 + w1 < Width ? w1 : Width - x1); // limit
+            h1 = (uint)(y1 + h1 < Height ? h1 : Height - y1); // limit
+            if ((w1 <= 0) || (h1 <= 0))
+                return false;
+            // make x1, w1 multiple of 8
+            w1 += x1 % 8;
+            if (w1 % 8 > 0) w1 += 8 - w1 % 8;
+            x1 -= x1 % 8;
+            SetParticalRam((UInt16)x1, (UInt16)y1, (UInt16)w1, (UInt16)h1);
+            SendCommand(0x22);
+            SendData(0xfc);
+            SendCommand(0x20);
+            return WaitReady(waitsec);
+        }
+
+        public override bool PerformFullRefresh()
+        {
+            SendCommand(0x1A);
+            SendData(0x64);
+            SendCommand(0x22);
+            SendData(0xd7);
+            SendCommand(32);
+            return WaitReady(waitsec);
+        }
+
+        protected override void SoftwareReset()
+        {
+            SendCommand(18);
+            WaitReady(waitsec, new CancellationTokenSource(1));
+        }
+
+        public bool WaitReady(CancellationToken cancellationToken = default)
+            => WaitUntilPinValueEquals(PinValue.Low, cancellationToken);
+
+        public bool WaitUntilPinValueEquals(
+        PinValue desiredValue,
+        CancellationToken cancellationToken = default)
+        {
+            while (!cancellationToken.IsCancellationRequested && _pioController.Read(_busyPin) != desiredValue)
+            {
+                cancellationToken.WaitHandle.WaitOne(5, true);
+            }
+
+            return !cancellationToken.IsCancellationRequested;
+        }
+    }
+}
